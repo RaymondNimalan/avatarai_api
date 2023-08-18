a8670bc9dac8be0b7e8569a7b2f56851af4f51f5
Ray Nimalan <raymondnimalan@gmail.com>
2023-08-17 21:30:13 -0400

    feat: post request to openai

--- index.ts
+++ index.ts
@@ -19,8 +19,33 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World From the Typescript Server!')
+app.get('/', async (req: Request, res: Response) => {
+  console.log('hi from get req')
+  const message = 'Hello World From the Typescript Server!'
+  res.json(message)
+});
+
+app.get('/models', async (req, res) => {
+  const response = await openai.listModels();
+  //console.log('response', response.data.data);
+  res.json(response.data.data);
+});
+
+app.post('/image', async (req, res) => {
+try {
+  const response = await openai.createImage({
+    prompt: "a white siamese cat",
+    n: 1,
+    size: "1024x1024",
+  });
+  const image_url = response.data.data[0].url;
+  res.json(image_url)
+  
+} catch (error) {
+  console.log('error with getting image post request', error)
+  res.send(error)
+  
+}
 });
 
 const port = process.env.PORT || 8000;
